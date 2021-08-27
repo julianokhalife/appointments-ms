d011aef746b1d9de4252e1b87b23ad6896e25e98
Juliano Khalife <khalifejuliano2@gmail.com>
2021-08-27 22:44:20 +0300

    chore: Added health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TerminusModule, HttpModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
